8a1dd8f7742e5b2afb712b4a27a3bbb990997639
Mathy <mathyptr@gmail.com>
2024-05-18 10:49:49 +0200

    update config script

--- configTensor.sh
+++ configTensor.sh
@@ -1,15 +1,15 @@
 cd /content
+#git clone https://github.com/mathyptr/tensorflow.git 
 git clone https://github.com/tensorflow/models.git 
-git clone https://github.com/mathyptr/tensorflow.git \
-wget https://raw.githubusercontent.com/hugozanini/object-detection/master/inferenceutils.py \
-mv  ./tensorflow/labelmap.pbtxt /content \
-mv ./tensorflow/kangaroodataset ./dataset \
-wget http://download.tensorflow.org/models/object_detection/classification/tf2/20200710/mobilenet_v2.tar.gz \
-tar -xvf mobilenet_v2.tar.gz \
-wget https://raw.githubusercontent.com/tensorflow/models/master/research/object_detection/configs/tf2/ssd_mobilenet_v2_320x320_coco17_tpu-8.config \
-mv ssd_mobilenet_v2_320x320_coco17_tpu-8.config mobilenet_v2.config \
-protoc /content/models/research/object_detection/protos/*.proto --python_out=/content/models/research/ \
-cp object_detection/packages/tf2/setup.py . \
+wget https://raw.githubusercontent.com/hugozanini/object-detection/master/inferenceutils.py 
+mv  ./tensorflow/labelmap.pbtxt /content 
+mv ./tensorflow/kangaroodataset ./dataset 
+wget http://download.tensorflow.org/models/object_detection/classification/tf2/20200710/mobilenet_v2.tar.gz 
+tar -xvf mobilenet_v2.tar.gz 
+wget https://raw.githubusercontent.com/tensorflow/models/master/research/object_detection/configs/tf2/ssd_mobilenet_v2_320x320_coco17_tpu-8.config 
+mv ssd_mobilenet_v2_320x320_coco17_tpu-8.config mobilenet_v2.config 
+protoc /content/models/research/object_detection/protos/*.proto --python_out=/content/models/research/ 
+cp object_detection/packages/tf2/setup.py . 
 python -m pip install .
 
 #WORKDIR /content
