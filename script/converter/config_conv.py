ff30e47a9842b44934fc99849cb3715c9f6f55ec
Mathilde <50731814+mathyptr@users.noreply.github.com>
2024-08-24 19:58:50 +0200

    Add files via upload

--- script/converter/config_conv.py
+++ script/converter/config_conv.py
@@ -3,4 +3,5 @@
 model_dir = '/content/model_web'
 inference_dir = '/content/inference_graph/saved_model'
 logfile_converter= '/content/log/converterlog.txt'
-webmodel_cmd='http://172.17.0.4/cgi-bin/startcmd.py'
+webmodel_srv='http://172.17.0.4/'
+startcmd=webmodel_srv+'cgi-bin/startcmd.py'
