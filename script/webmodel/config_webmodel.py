e49e658a918227790f5626a06b161c8d5040b07b
Mathilde <50731814+mathyptr@users.noreply.github.com>
2024-08-24 19:59:13 +0200

    Add files via upload

--- script/webmodel/config_webmodel.py
+++ script/webmodel/config_webmodel.py
@@ -4,6 +4,7 @@ model_dir = '/content/model'
 
 logfile_webmodel= '/content/log/webmodellog.txt'
 logfile_wget= '/content/log/wget.txt'
-model_URL='http://172.17.0.2/model_web/'
+converter_srv=''http://172.17.0.2/''
+model_URL=converter_srv+'model_web/'
 
 
