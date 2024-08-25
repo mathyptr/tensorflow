e399829b55b4323d65b9bb62cd47a610843ac860
Mathilde <50731814+mathyptr@users.noreply.github.com>
2024-08-25 19:43:59 +0200

    Add files via upload

--- script/webmodel/webmodel_main.py
+++ script/webmodel/webmodel_main.py
@@ -8,8 +8,8 @@ import time
 file_cmd= '/content/cmd/cmdfile.txt'
 script_dir='/content/webmodel/'
 
-cmddownload=[sys.executable, script_dir+'download_dir.py']
-print("CMD: ",cmddownload)
+download_cmd=[sys.executable, script_dir+'download_dir.py']
+print("CMD: ",download_cmd)
 while True:
     try:
         f = open(file_cmd, "r")
@@ -17,21 +17,23 @@ while True:
         if line.find("START")>-1 :
             f.close()        
             print("FIND CMD START")
-            print("START DOWNLOAD MODEL")
+
+            print("START DOWNLOAD DATA")
             f = open(file_cmd, "w")
-            f.writelines("START DOWNLOAD MODEL")
+            f.writelines("START DOWNLOAD DATA")
             f.close()
 
-            with subprocess.Popen(cmddownload, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True) as proc:
+            with subprocess.Popen(download_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True) as proc:
                 for line in proc.stderr:
                  print(line)
                 stdout, stderr = proc.communicate()
-            result = subprocess.CompletedProcess(cmddownload, proc.returncode, stdout, stderr)
+            result = subprocess.CompletedProcess(download_cmd, proc.returncode, stdout, stderr)
 
-            print("DOWNLOAD MODEL EXECUTED")
+            print("DOWNLOAD DATA EXECUTED")
             f = open(file_cmd, "w")
-            f.writelines("DOWNLOAD MODEL EXECUTED")
+            f.writelines("DOWNLOAD DATA EXECUTED")
             f.close()
+
 
         else:
             f.close()
