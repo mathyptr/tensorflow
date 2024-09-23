a1ef2102966a5cdeb1ec12af942334574c160454
Mathilde <50731814+mathyptr@users.noreply.github.com>
2024-09-23 23:58:50 +0200

    Add files via upload

--- script/augmentation/split_train_test.py
+++ script/augmentation/split_train_test.py
@@ -1,26 +1,26 @@
-
-
 from config import *
-
+import random 
 
 augmented_file=imageout_dir+'/augmented_files.csv'
+train_file=work_dir+'/train_labels.csv'
+test_file=work_dir+'/test_labels.csv'
 
-nlines=0
-for line in open(augmented_file):
-    nlines+=1
+f = open(augmented_file,'r')
+header_csv = f.readline()
+lines = f.readlines()
+f.close()
 
-nlines-=1
 
-train_file=imageout_dir+'/train_labels.csv'
-test_file=imageout_dir+'/test_labels.csv'
-
+nlines=len(lines)
+random.shuffle(lines) 
 ftrain = open(train_file, "w")
 ftest = open(test_file, "w")
-header_csv = 'filename,width,height,class,xmin,ymin,xmax,ymax,source' 
-                   
+
+ftrain.writelines(header_csv)                   
 ftest.writelines(header_csv)
+
 i=0
-for line in open(augmented_file):
+for line in lines:
     if i<=nlines*80/100 :
         ftrain.writelines(line)
     else:
