f15e5a49e084b1a2b45584da8d2cf7718f651342
Mathilde <50731814+mathyptr@users.noreply.github.com>
2024-10-02 22:41:53 +0200

    Update config.py

--- script/webmodel/config.py
+++ script/webmodel/config.py
@@ -17,16 +17,7 @@ tmp_dir='/content/tmp/'
 download_dir=tmp_dir+remote_path
 work_dir=home_dir+remote_path
 token_in_file=download_dir+'token.txt'
+out_dir = '/content/model_web/'
+token_out_file=out_dir +'token.txt
 
 mysecret="mx4JncNOPY0ZwvU1B7re6KGH3GDTULs6"
-
-
-
-
-
-
-
-
-token_in_file=download_dir+'token.txt'
-out_dir = '/content/model_web/'
-token_out_file=out_dir +'token.txt'
